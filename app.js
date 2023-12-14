e0059e2d7e7b8d1085d01ad5a30ab05d85cea03c
Tony Kalalian <120026287+tonykalalian@users.noreply.github.com>
2023-12-14 14:51:33 +0200

    First

---
+++ app.js
@@ -0,0 +1,31 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
+require("dotenv").config();
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType,
+    },
+  };
+}
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+  const prompt = "What's different between these pictures?";
+
+  const imageParts = [
+    fileToGenerativePart("images/1.jpg", "image/jpeg"),
+    fileToGenerativePart("images/2.jpg", "image/jpeg"),
+  ];
+
+  const result = await model.generateContent([prompt, ...imageParts]);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require("dotenv").config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const app = express(); // Move this up
+
+async function run() {
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    const prompt = "Write me a love message for my girlfriend";
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  } catch (error) {
+    console.error("Error in run():", error);
+  }
+}
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  run();
+});
